dcc23299c3062887d516b87b593f6bee9ee864d2
Tomáš Mîčka <tomas.micka@logmanager.com>
2025-01-06 16:41:11 +0100

    update

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,33 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+-- Window settings
+config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = "RESIZE"
+config.window_padding = { left = 5, right = 0, top = 5, bottom = 0 }
+
+-- Font settings
+config.font = wezterm.font 'Iosevka NFM'
+config.font_size = 12.8
+
+config.keys = {
+	{
+		key = "l",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.DisableDefaultAssignment,
+	},{
+		key = "e",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.DisableDefaultAssignment,
+	},{
+		key = "Space",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.DisableDefaultAssignment,
+	},{
+		key = "Space",
+		mods = "CTRL",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+}
+
+return config
